d21d8291cdecb438344450e3d66ffca5d05ee35a
Lucas Carvalho <lucasprocopio.carvalho@gmail.com>
2016-05-30 23:34:34 -0300

    DAO e COntroller do Turno.

---
+++ CAS/comum/cas/comum/controller/TurnoController.java
@@ -0,0 +1,128 @@
+package cas.comum.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import cas.comum.dao.TurnoDAO;
+import cas.comum.dominio.Turno;
+
+public class TurnoController {
+
+	public void selecionarOperacao(){
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("####### Gerenciar Turnos #######");
+		System.out.println("---------------------------------------");
+		System.out.println("Por favor digite uma opção desejada:");
+		System.out.println("1 - Cadastrar um novo Turno.;");
+		System.out.println("2 - Listar Turno(s) existente(s).;");
+		System.out.println("3 - Remover Turno Existente.;");
+		System.out.println("9 - << Voltar.;");
+		System.out.println("---------------------------------------");
+		
+		String operacao = entrada.nextLine();
+		
+		switch (operacao) {
+		case "1":preCadastrarTurno(); break;
+		case "2":listarTurnos(); break;
+		case "3":preRemoverTurno(); break;
+		case "9":voltar(); break;
+		default: System.out.println("Opção selecionada inexistente"); 
+				 selecionarOperacao();
+				 break;
+		}
+	}
+	
+	/**
+	 * Pre cadastrar sala de aula
+	 */
+	private void preCadastrarTurno(){
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("####### Cadastrar Turno #######");
+		System.out.println("---------------------------------------");
+		System.out.println("Por favor digite a descrição do turno a inserir (Manhã, Tarde e etc...):");
+		String descricao = entrada.nextLine();
+		System.out.println("---------------------------------------");
+		
+		if(!descricao.isEmpty()){
+			entrada.close();
+			cadastrarTurno(descricao);
+		}else{
+			System.out.println("\n**Descrição: campo obrigatório não informado. \n");
+			entrada.close();
+			preCadastrarTurno();
+		}
+		
+	}
+	
+	/**
+	 * Cadastrar sala de aula
+	 * @param descricao
+	 */
+	private void cadastrarTurno(String descricao){
+		TurnoDAO dao = new TurnoDAO();
+		dao.save("INSERT INTO comum.turno "
+				+ "(descricao) "
+				+ "VALUES (?)", descricao);
+		
+		System.out.println("<<Cadastro realizado com sucesso.>>");
+		selecionarOperacao();
+	}
+	
+	//Lista de Turnos
+	private void listarTurnos(){
+		TurnoDAO dao = new TurnoDAO();
+		List<Turno> turnos = new ArrayList<Turno>();
+		int count = 0;
+		
+		turnos = dao.findTurnos();
+		
+		if(!turnos.isEmpty()){
+			System.out.println("### Lista de Turnos: ###");
+			for(Turno turno : turnos){
+				count++;			
+				System.out.println("#"+count+" - "+turno.getDescricao());
+			}
+		}else{
+			System.out.println("Nenhum turno encontrado.");
+		}
+	}
+	
+	private void preRemoverTurno(){
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("####### Remover Turno #######");
+		System.out.println("---------------------------------------");
+		System.out.println("Por favor digite a descrição do turno que deseja remover (Manhã, Tarde e etc...):");
+		String descricao = entrada.nextLine();
+		System.out.println("---------------------------------------");
+		
+		if(!descricao.isEmpty()){
+			entrada.close();
+			removerTurno(descricao);
+		}else{
+			System.out.println("\n**Descrição: campo obrigatório não informado. \n");
+			entrada.close();
+			preCadastrarTurno();
+		}
+	}
+	
+	private void removerTurno(String desc){
+		TurnoDAO dao = new TurnoDAO();
+		
+		String sql = "DELETE FROM comum.turno WHERE descricao = ?";
+
+		dao.delete(sql, desc);
+		
+		System.out.println("Turno removido com sucesso.");
+		selecionarOperacao();
+	}
+	
+	private void voltar(){
+		
+	}
+	
+}
--- CAS/comum/cas/comum/dao/TurnoDAO.java
+++ CAS/comum/cas/comum/dao/TurnoDAO.java
@@ -6,17 +6,45 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import cas.comum.dominio.Turno;
+import cas.espacoFisico.dao.SalaDao;
 import cas.util.util.GenericDao;
 
 /*
  * @author Lucas Carvalho
  */
 public class TurnoDAO extends GenericDao{
+	
+	//Busca, através do identificador, um turno cadastrado
+	public Turno findTurnobyId(Integer id){
+		List<Turno> turnos = new ArrayList<Turno>();
+		Connection con = getConnection();
+		
+		String sql = "SELECT * FROM comum.turno WHERE id_turno = ?";
+		
+		try{
+			PreparedStatement stm = con.prepareStatement(sql);
+			stm.setInt(1, id);
+			
+			ResultSet rs = stm.executeQuery();
+			
+			while (rs.next()){
+				turnos.add(populaTurno(rs));
+			}
 
-	//Busca os turnos cadastrados
-	public List<Turno> findTurnobyDescricao(String desc){
+			if (!turnos.isEmpty())
+				return turnos.get(0);
+			else
+				return new Turno();
+		}catch (SQLException e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+	//Busca um turno pela descrição
+	public Turno findTurnobyDescricao(String desc){
 		List<Turno> turnos = new ArrayList<Turno>();
 		Connection con = getConnection();
 		
@@ -29,18 +57,56 @@ public class TurnoDAO extends GenericDao{
 			ResultSet rs = stm.executeQuery();
 			
 			while (rs.next()){
-				Turno turno = new Turno();
-				
-				turno.setId(rs.getInt("id_turno"));
-				turno.setDescricao(rs.getString("descricao"));
-		
-				turnos.add(turno);
+				turnos.add(populaTurno(rs));
 			}
-			 
-			
-			return turnos;
+
+			if (!turnos.isEmpty())
+				return turnos.get(0);
+			else
+				return new Turno();
 		}catch (SQLException e){
 			throw new RuntimeException(e);
 		}
+	}
+
+	//Busca todos os turnos cadastrados
+	public List<Turno> findTurnos(){
+		List<Turno> turnos = new ArrayList<Turno>();
+		Connection con = getConnection();
+		
+		String sql = "SELECT * FROM comum.turno";
+		
+		try{
+			PreparedStatement stm = con.prepareStatement(sql);
+			
+			ResultSet rs = stm.executeQuery();
+			
+			while (rs.next()){
+				turnos.add(populaTurno(rs));
+			}
+
+			if (!turnos.isEmpty())
+				return turnos;
+			else
+				return null;
+		}catch (SQLException e){
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	/* Método utilizado para popular as coleções de turnos, obtidas nas consultas ao banco.
+	 * 
+	 * @param rs
+	 * 
+	 * @return turno
+	 */
+	private Turno populaTurno(ResultSet rs) throws SQLException{
+		Turno turno = new Turno();
+		
+		turno.setId(rs.getInt("id_turno"));
+		turno.setDescricao(rs.getString("descricao"));
+		
+		return turno;
 	}
 }
