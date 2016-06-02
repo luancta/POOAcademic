f61229f0c8a34bd3a7dc49b994a8d10a16da6cb9
Gilley Lopes <gilleylopes@gmail.com>
2016-06-02 18:16:57 -0300

    CRUD Reserva incompleto / terminar data

--- CAS/espacoFisico/cas/espacoFisico/controller/ReservaController.java
+++ CAS/espacoFisico/cas/espacoFisico/controller/ReservaController.java
@@ -1,24 +1,32 @@
 package cas.espacoFisico.controller;
 
+import java.util.List;
 import java.util.Scanner;
 
 import cas.acesso.controller.MenuController;
+import cas.ensino.dao.TurmaDao;
+import cas.ensino.dominio.Turma;
+import cas.espacoFisico.dao.LocalAulaDao;
 import cas.espacoFisico.dao.ReservaDao;
+import cas.espacoFisico.dominio.LocalAula;
 import cas.espacoFisico.dominio.Reserva;
 import cas.util.util.ViewConsoleUtil;
 
 /**
- * Controlador de Reserva de 
+ * Controlador de Reserva de
  * 
  * @author Gilley
  *
  */
 public class ReservaController {
 
+	private Reserva reserva;
+
 	/**
 	 * Selecionar operação a ser realizada
-	 * @throws Exception 
-	 * @throws NumberFormatException 
+	 * 
+	 * @throws Exception
+	 * @throws NumberFormatException
 	 */
 	public void selecionarOperacao() throws NumberFormatException, Exception {
 
@@ -54,31 +62,228 @@ public class ReservaController {
 	}
 
 	/**
-	 * Pre cadastrar reserva de 
-	 * @throws Exception 
-	 * @throws NumberFormatException 
+	 * Pre cadastrar reserva de
+	 * 
+	 * @throws Exception
+	 * @throws NumberFormatException
 	 */
 	private void preCadastrarReserva() throws NumberFormatException, Exception {
 
 		Scanner entrada = new Scanner(System.in);
-		Reserva reserva = new Reserva();
+		reserva = new Reserva();
+		reserva.setAtivo(true);
 
 		ViewConsoleUtil.limparConsole();
 		ViewConsoleUtil.setDivisor();
 		ViewConsoleUtil.setBreadCrumb("Cadastrar Reserva");
 		ViewConsoleUtil.setDivisor();
 
-		//DO METODOS PARA INFORMAR
+		// DO METODOS PARA INFORMAR
+		informarUsaProjetor(entrada);
+		informarData(entrada);
+		informarTurma(entrada);
+		informarLocalAula(entrada);
 
 		cadastrarReserva(reserva);
 	}
 
 	/**
-	 * Cadastrar reserva de 
+	 * Informar data de reserva
+	 * 
+	 * @param entrada
+	 */
+	private void informarData(Scanner entrada) {
+		// TODO LUCAS ESTA DESENVOLVENDO
+
+	}
+
+	/**
+	 * Informar caso reserva utiliza projetor
+	 * 
+	 * @param entrada
+	 */
+	private void informarUsaProjetor(Scanner entrada) {
+
+		ViewConsoleUtil.setMensagemOpcao("Por favor informe se a reserva usa projetor");
+		ViewConsoleUtil.setOpcao(1, "Sim");
+		ViewConsoleUtil.setOpcao(2, "Não");
+		String usaProjetor = entrada.nextLine();
+		ViewConsoleUtil.setDivisor();
+
+		if (usaProjetor.isEmpty()) {
+			ViewConsoleUtil.setMensagemErro("Usa projetor: campo obrigatório não informado.");
+			informarUsaProjetor(entrada);
+		}
+
+		switch (usaProjetor) {
+		case "1":
+			reserva.setUsaProjetor(true);
+			break;
+		case "2":
+			reserva.setUsaProjetor(false);
+			break;
+		default:
+			System.out.println("Opção informada inexistente");
+			informarUsaProjetor(entrada);
+			break;
+		}
+
+	}
+
+	/**
+	 * Informar Turma
+	 * 
+	 * @param entrada
+	 * @throws NumberFormatException
+	 * @throws Exception
+	 */
+	private void informarTurma(Scanner entrada) throws NumberFormatException, Exception {
+
+		listarTurmas();
+
+		ViewConsoleUtil.setMensagemOpcao("Por favor informe o código da turma correspondente");
+		String turma = entrada.nextLine();
+		ViewConsoleUtil.setDivisor();
+
+		String regex = "\\d+";
+		if (!turma.matches(regex)) {
+			ViewConsoleUtil.setMensagemErro("Por favor informe uma Turma válida.");
+			informarTurma(entrada);
+		}
+
+		TurmaDao turmaDao = new TurmaDao();
+		reserva.setTurma(turmaDao.findById(Integer.parseInt(turma)));
+	}
+
+	/**
+	 * Listar Turmas
+	 * 
+	 * @throws Exception
+	 * @throws NumberFormatException
+	 */
+	private void listarTurmas() throws NumberFormatException, Exception {
+
+		Scanner entrada = new Scanner(System.in);
+
+		ViewConsoleUtil.limparConsole();
+		ViewConsoleUtil.setDivisor();
+		ViewConsoleUtil.setBreadCrumb("Listar Turmas");
+		ViewConsoleUtil.setDivisor();
+		ViewConsoleUtil.setMensagemOpcao("Por favor informe o código da turma de aula");
+		String filtro = entrada.nextLine();
+
+		buscarTurma(filtro);
+	}
+
+	/**
+	 * Buscar e listar Turma
+	 * 
+	 * @param filtro
+	 * @throws Exception
+	 * @throws NumberFormatException
+	 */
+	private void buscarTurma(String filtro) throws NumberFormatException, Exception {
+
+		TurmaDao dao = new TurmaDao();
+		List<Turma> resultado = dao.findByCodigo(filtro);
+		ViewConsoleUtil.setDivisor();
+
+		if (resultado.isEmpty()) {
+			ViewConsoleUtil.setMensagemErro("Nenhum registro foi encontrado");
+			selecionarOperacao();
+		} else {
+			ViewConsoleUtil.setTabelaHead("id", "Código");
+			for (Turma turma : resultado) {
+				System.out.println("");
+				ViewConsoleUtil.setTabelaItem(String.valueOf(turma.getId()), turma.getCodigo());
+			}
+
+			System.out.println("");
+			System.out.println("Total de " + resultado.size() + " turmas encontradas.");
+			System.out.println("");
+		}
+	}
+
+	/**
+	 * Informar LocalAula
+	 * 
+	 * @param entrada
+	 * @throws NumberFormatException
+	 * @throws Exception
+	 */
+	private void informarLocalAula(Scanner entrada) throws NumberFormatException, Exception {
+
+		listarLocalAulas();
+
+		ViewConsoleUtil.setMensagemOpcao("Por favor informe o bloco do local Aula correspondente");
+		String bloco = entrada.nextLine();
+		ViewConsoleUtil.setDivisor();
+
+		String regex = "\\d+";
+		if (!bloco.matches(regex)) {
+			ViewConsoleUtil.setMensagemErro("Por favor informe um Local Aula válido.");
+			informarLocalAula(entrada);
+		}
+
+		LocalAulaDao localAulaDao = new LocalAulaDao();
+		reserva.setLocalAula(localAulaDao.findById(Integer.parseInt(bloco)));
+	}
+
+	/**
+	 * Listar LocalAulas
+	 * 
+	 * @throws Exception
+	 * @throws NumberFormatException
+	 */
+	private void listarLocalAulas() throws NumberFormatException, Exception {
+
+		Scanner entrada = new Scanner(System.in);
+
+		ViewConsoleUtil.limparConsole();
+		ViewConsoleUtil.setDivisor();
+		ViewConsoleUtil.setBreadCrumb("Listar Local Aulas");
+		ViewConsoleUtil.setDivisor();
+		ViewConsoleUtil.setMensagemOpcao("Por favor informe o bloco do local Aula de aula");
+		String filtro = entrada.nextLine();
+
+		buscarLocalAula(filtro);
+	}
+
+	/**
+	 * Buscar e listar LocalAula
+	 * 
+	 * @param filtro
+	 * @throws Exception
+	 * @throws NumberFormatException
+	 */
+	private void buscarLocalAula(String filtro) throws NumberFormatException, Exception {
+
+		LocalAulaDao dao = new LocalAulaDao();
+		List<LocalAula> resultado = dao.findByBloco(filtro);
+		ViewConsoleUtil.setDivisor();
+
+		if (resultado.isEmpty()) {
+			ViewConsoleUtil.setMensagemErro("Nenhum registro foi encontrado");
+			selecionarOperacao();
+		} else {
+			ViewConsoleUtil.setTabelaHead("id", "Bloco");
+			for (LocalAula localAula : resultado) {
+				System.out.println("");
+				ViewConsoleUtil.setTabelaItem(String.valueOf(localAula.getId()), localAula.getBloco());
+			}
+
+			System.out.println("");
+			System.out.println("Total de " + resultado.size() + " local Aula encontrados.");
+			System.out.println("");
+		}
+	}
+
+	/**
+	 * Cadastrar reserva de
 	 * 
 	 * @param tombo
-	 * @throws Exception 
-	 * @throws NumberFormatException 
+	 * @throws Exception
+	 * @throws NumberFormatException
 	 */
 	private void cadastrarReserva(Reserva reserva) throws NumberFormatException, Exception {
 		ReservaDao dao = new ReservaDao();
@@ -88,16 +293,17 @@ public class ReservaController {
 	}
 
 	/**
-	 * Listar reserva de 
-	 * @throws Exception 
-	 * @throws NumberFormatException 
+	 * Listar reserva de
+	 * 
+	 * @throws Exception
+	 * @throws NumberFormatException
 	 */
 	private void listarReservas() throws NumberFormatException, Exception {
 
 		Scanner entrada = new Scanner(System.in);
 
 		ViewConsoleUtil.limparConsole();
-		ViewConsoleUtil.setBreadCrumb("Listar Reserva de ");
+		ViewConsoleUtil.setBreadCrumb("Listar Reserva");
 		ViewConsoleUtil.setDivisor();
 		ViewConsoleUtil.setMensagemOpcao("Por favor informe o número do reserva de ");
 		String filtro = entrada.nextLine();
@@ -106,71 +312,57 @@ public class ReservaController {
 	}
 
 	/**
-	 * Buscar e listar reserva de 
+	 * Buscar e listar reserva de
 	 * 
 	 * @param filtro
-	 * @throws Exception 
-	 * @throws NumberFormatException 
+	 * @throws Exception
+	 * @throws NumberFormatException
 	 */
 	private void buscar(String filtro) throws NumberFormatException, Exception {
 
 		ReservaDao dao = new ReservaDao();
-		Reserva resultado = dao.findById( Integer.parseInt(filtro) );
+		Reserva resultado = dao.findById(Integer.parseInt(filtro));
 
 		ViewConsoleUtil.setDivisor();
 
-/*		if (resultado.isEmpty()) {
-			ViewConsoleUtil.setMensagemErro("Nenhum registro foi encontrado");
-			selecionarOperacao();
-		} else {
-			ViewConsoleUtil.setTabelaHead("id", "bloco", "capacidade", "sala", "laboratório", "Operação");
-				System.out.println("");
-				ViewConsoleUtil.setTabelaItem(String.valueOf(resultado.getId()), resultado.getBloco(),
-						String.valueOf(resultado.getCapacidade()),
-						resultado.getSala() != null ? resultado.getSala().getNumero() : "",
-								resultado.getLaboratorio() != null ? resultado.getLaboratorio().getNome() : "",
-						"R - Remover / B - Buscar / V - Voltar");
-			}
-
-			System.out.println("");
-			System.out.println("Total de " + resultado.size() + " reserva de s encontrados.");
-			System.out.println("");
-		}
-
-		Scanner entrada = new Scanner(System.in);
-
-		ViewConsoleUtil.setMensagemOpcao("Por favor informe a operação desejada");
-		String operacao = entrada.nextLine();
-
-		String regex = "\\d+";
-		if (!operacao.matches(regex))
-			operacao = operacao.toUpperCase();
-		else {
-			ViewConsoleUtil.setMensagemErro("A opção informada não pode ser um número");
-			buscar(filtro);
-		}
-
-		switch (operacao) {
-		case "R":
-			preRemover();
-			break;
-		case "B":
-			listarReservas();
-			break;
-		case "V":
-			selecionarOperacao();
-			break;
-		default:
-			ViewConsoleUtil.setMensagemErro("Opção selecionada inexistente");
-			buscar(filtro);
-			break;
-		}*/
+		/*
+		 * if (resultado.isEmpty()) { ViewConsoleUtil.setMensagemErro(
+		 * "Nenhum registro foi encontrado"); selecionarOperacao(); } else {
+		 * ViewConsoleUtil.setTabelaHead("id", "bloco", "capacidade", "sala",
+		 * "laboratório", "Operação"); System.out.println("");
+		 * ViewConsoleUtil.setTabelaItem(String.valueOf(resultado.getId()),
+		 * resultado.getBloco(), String.valueOf(resultado.getCapacidade()),
+		 * resultado.getSala() != null ? resultado.getSala().getNumero() : "",
+		 * resultado.getLaboratorio() != null ?
+		 * resultado.getLaboratorio().getNome() : "",
+		 * "R - Remover / B - Buscar / V - Voltar"); }
+		 * 
+		 * System.out.println(""); System.out.println("Total de " +
+		 * resultado.size() + " reserva de s encontrados.");
+		 * System.out.println(""); }
+		 * 
+		 * Scanner entrada = new Scanner(System.in);
+		 * 
+		 * ViewConsoleUtil.setMensagemOpcao(
+		 * "Por favor informe a operação desejada"); String operacao =
+		 * entrada.nextLine();
+		 * 
+		 * String regex = "\\d+"; if (!operacao.matches(regex)) operacao =
+		 * operacao.toUpperCase(); else { ViewConsoleUtil.setMensagemErro(
+		 * "A opção informada não pode ser um número"); buscar(filtro); }
+		 * 
+		 * switch (operacao) { case "R": preRemover(); break; case "B":
+		 * listarReservas(); break; case "V": selecionarOperacao(); break;
+		 * default: ViewConsoleUtil.setMensagemErro(
+		 * "Opção selecionada inexistente"); buscar(filtro); break; }
+		 */
 
 	}
 
 	/**
-	 * Pre remover reserva de 
-	 * @throws Exception 
+	 * Pre remover reserva de
+	 * 
+	 * @throws Exception
 	 */
 	private void preRemover() throws Exception {
 
@@ -189,11 +381,11 @@ public class ReservaController {
 	}
 
 	/**
-	 * Remover Reserva de 
+	 * Remover Reserva de
 	 * 
 	 * @param id
-	 * @throws Exception 
-	 * @throws NumberFormatException 
+	 * @throws Exception
+	 * @throws NumberFormatException
 	 */
 	private void remover(int id) throws NumberFormatException, Exception {
 
@@ -211,7 +403,8 @@ public class ReservaController {
 
 	/**
 	 * Voltar para aréa administrativa
-	 * @throws Exception 
+	 * 
+	 * @throws Exception
 	 */
 	private void voltar() throws Exception {
 
